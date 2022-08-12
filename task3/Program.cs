a08b8bb4b9ea3a808108c49c2a67decd05c85cd4
Svetlana Gluschenko <lanasvetlaya2021@mail.ru>
2022-08-12 19:39:11 +0400

    Добавила 3 задание

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿   class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         Console.WriteLine("Введите число : ");
+         int a = Convert.ToInt32(Console.ReadLine());
+         
+         string res = a%2==1?"Нет":"Да";
+         
+         Console.WriteLine("Число {0} -> {1}",a,res);
+         
+        }
+    }
