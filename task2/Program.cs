0b5233d02c2468b6b5cb0256fcdfafed2319a6a9
Svetlana Gluschenko <lanasvetlaya2021@mail.ru>
2022-08-12 19:33:16 +0400

    Добавила 2е задание

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         Console.WriteLine("Введите число а : ");
+         int a = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Введите число b : ");
+         int b = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Введите число с : ");
+         int c = Convert.ToInt32(Console.ReadLine());
+         
+          
+         int maxd = a>b?a:b;
+             maxd = maxd>c?maxd:c;            
+ 
+          Console.WriteLine("{0};{1};{2} -> {3}",a,b,c,maxd);
+         
+        }
+    }
