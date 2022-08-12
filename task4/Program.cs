8320db6fa9c538b153e0ce55a0284c0721a7c3fe
Svetlana Gluschenko <lanasvetlaya2021@mail.ru>
2022-08-12 19:44:55 +0400

    Добавила 4е задание

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         Console.WriteLine("Введите число : ");
+         int a = Convert.ToInt32(Console.ReadLine());
+         string result="";
+        for (int count =1 ; count!=a+1; count++) 
+         {
+          if (count%2!=1) 
+          {
+            if (count>2 && a%2==1) result=result+",";
+            if (count>3 && a%2!=1) result=result+",";    
+            result=result+count.ToString();
+            
+          }
+          
+         }
+         
+         Console.WriteLine("Число {0} -> {1}",a,result);
+         
+        }
+    }
