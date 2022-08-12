3635525c14ed32e7f12f1eeac4116e4d811cc2e3
Svetlana Gluschenko <lanasvetlaya2021@mail.ru>
2022-08-12 19:02:35 +0400

    Добавила 1 задание

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+      {
+         
+         Console.WriteLine("Введите число а : ");
+         int a = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Введите число b : ");
+         int b = Convert.ToInt32(Console.ReadLine());
+         
+          
+         int maxd = a>b?a:b;   
+ 
+          Console.WriteLine("a = {0};b = {1} -> max = {2}",a,b,maxd);
+         
+        }
+    }
+ 
+
+ 
