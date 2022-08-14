68ddfc652f6eef87d617825bc270ec7a30d55afb
Svetlana Gluschenko <lanasvetlaya2021@mail.ru>
2022-08-14 20:10:40 +0400

    Added Task6

---
+++ task6/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+   /*
+   Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+    645 -> 5
+    78 -> третьей цифры нет
+    32679 -> 6
+   */
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Введите число");
+            string? digit = Console.ReadLine(); 
+            digit = digit.Length==0?"0":digit;            
+            
+            string digit_s=Convert.ToString(Math.Abs(Convert.ToInt32(digit))); // Полученную строку конвертим в число берем моуль и конвертим обратно в строку . Для обработки чисел <0 
+           if (digit_s.Length>=3) // Проверяем что число не менее чем 3х значное    
+            {
+              Console.WriteLine($"{digit}->{digit_s[2]}");
+            }
+           else
+            {
+               Console.WriteLine($"{digit}->третьей цифры нет");
+               return;
+            }
+        }
+    }
